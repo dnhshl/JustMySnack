5c08ab6487da1a4330a7d25f22772b64ef289416
dnhshl <58460904+dnhshl@users.noreply.github.com>
2021-01-13 12:47:11 +0100

    Initiale Version

---
+++ app/src/main/java/com/example/justmybutton/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.justmybutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    val button: Button by lazy {findViewById(R.id.button)}
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
